d79ec9db387f93dec3deb6166eb00ef99d898581
augusto <augustosoaresd232@gmail.com>
2025-11-23 21:33:43 -0400

    teste2 para o site Aurus

--- pages/index.js
+++ pages/index.js
@@ -168,7 +168,7 @@ main .input{
           </div>
 
           <div className="botao">
-            <a href="aurus.tec.br">
+            <a href="https://novo-ruby.vercel.app/">
               <button type="button">Continuar</button>
             </a>
           </div>
